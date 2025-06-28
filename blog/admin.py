2b2c0a8d68e9456433871c4a6c2553c914f01467
KarolOsorio <kosoriom@unal.edu.co>
2025-06-28 11:18:22 -0500

    Mi aplicación Django Girls, primer commit

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post) #Para hacer nuestro modelo visible en la página del administrador, tenemos que registrar el modelo.
+
+# Register your models here.
